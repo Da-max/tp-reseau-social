6bec39f4c1bd398aecd0bdaa7ad1e0a91512b332
BEN HASSEN MAXIME p2007158 <p2007158@lifasr2.univ-lyon1.fr>
2021-03-09 16:15:10 +0100

    First change

--- fonctions-res.php
+++ fonctions-res.php
@@ -1,6 +1,13 @@
 <?php
 
+require_once '../../LIB/librairie-res.php';
+
 function retourne_auteur()
 {
     return explode('/', $_SERVER['REQUEST_URI'])[2];
+}
+
+function ajout_post_msg() {
+	$content = $_POST['post-msg'] . retourne_auteur() . date();
+	file_put_contents($nbPost . '-msg.txt');
 }
